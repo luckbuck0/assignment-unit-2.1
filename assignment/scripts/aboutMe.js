0465242ffa71180f0e989597794138a91f0b7ff9
Luckie Bah <bah966@gmail.com>
2023-03-01 17:02:54 +0000

    Finished

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -33,13 +33,19 @@ const allowedPets= 3
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if (luckyNumber===2 && adventurous===true) { console.log('Roll the dice!"')
+    
+}
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets<allowedPets) { console.log ('I can have more pets!')
+    }
+else if (pets===allowedPets) { console.log('I have enough pets')}
+else if (pets<allowedPets) {console.log('Oh no, I have too many pets!')}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -47,14 +53,27 @@ const allowedPets= 3
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-
+let mostPets= pets + friendsPets
+console.log(mostPets)
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
-
+switch (luckyNumber) {
+    case 1:  console.log('First is the worst')
+    break;
+    case 2: console.log('Second is the best')
+    break;
+    case 3: console.log('Third is the one with the polka dot dress')
+    break;
+    default: console.log('Luck is what happens when preparation meets opportunity')
+    break;
+};
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+{ String = adventurous=true ? 'Adventures are great!' : 'How about we stay home?'}
+console.log(adventurous);
+
 
 
