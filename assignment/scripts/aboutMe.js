db9313a8e65cdf2d190f035a04ca3ae17ce24b47
Luckie Bah <bah966@gmail.com>
2023-03-12 11:44:32 +0000

    corrections

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,10 +2,10 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName= 'Luckie'
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-let lastName= 'Bah'
+let lastName= ' Bah'
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName= firstName + lastName
+let fullName= (firstName + '' + (lastName));
 // 4 - Console log the value of `fullName`
 console.log(fullName)
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
@@ -15,15 +15,15 @@ let luckyNumber= 7
 // Refer back to the videos if you need help with this one.
 console.log('my name is ' + fullName + ' and I think ' + luckyNumber + ' is a winner!')
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous= true
+let adventurous= false
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food= ('sushi','tacos','chicken')
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets= 1
+let pets= 2
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets= 2
+let friendsPets= 5
 // 11 - Add two pets to your `pets` variable
-pets =+ 2
+pets= pets + 1
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets= 3
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
@@ -53,8 +53,12 @@ else if (pets>allowedPets) {console.log('Oh no, I have too many pets!')}
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-let mostPets= pets + friendsPets
-console.log(mostPets)
+let mostPets;
+if(friendsPets>pets) {(mostPets=friendsPets)}
+else if (pets>friendsPets) {(mostPets=pets)}
+else if (friendsPets===pets) {(mostPets=pets)}
+console.log(mostPets);
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
@@ -72,8 +76,13 @@ switch (luckyNumber) {
     break;
 };
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-{ String = adventurous=true ? 'Adventures are great!' : 'How about we stay home?'}
-console.log(adventurous);
 
+let adv =
+adventurous === true
+    ? "Adventures are great!"
+    : adventurous === false
+    ? "How about we stay home?"
+    :adventurous 
+console.log(adv)
 
 
